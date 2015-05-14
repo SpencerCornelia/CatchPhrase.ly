70f187f13a1e9d283f628a7080f24577f8f3b435
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-05-14 09:01:57 -0700

    stuck

--- index.js
+++ index.js
@@ -3,17 +3,55 @@
 //require all of our libraries
 //do all of our routing (app.get, app.listen)
 
-// requirements
-var express = require('express'),
-    app = express();
+// REQUIREMENTS //
+var express = require("express"),
+    app = express(),
+    path = require("path"),
+    _ = require("underscore"),
+    bodyParser = require("body-parser");
 
-// a "GET" request to "/" will run the function below
-app.get("/", function (req, res) {
-    // send back the response: 'Hello World'
-    res.send("Home page");
+// CONFIG //
+
+// serve js & css files into a public folder
+app.use(express.static(__dirname + '/public'));
+
+// body parser config
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// DATA //
+
+// pre-seeded phrase data
+var phrases =[
+  {id: 0, word: "spencer", definition: "cornelia"},
+  {id: 1, word: "spencer1", definition: "cornelia2"},
+  {id: 2, word: "spencer2", definition: "cornelia3"},
+  {id: 3, word: "spencer3", definition: "cornelia4"},
+  {id: 4, word: "spencer4", definition: "cornelia5"}
+];
+
+// ROUTES //
+
+// root path
+app.get("/", function (req, res){
+  // render home.html
+  res.sendFile(path.join(__dirname + '/public/html/home.html'));
 });
 
-// start the server
-app.listen(3000, function () {
-    console.log("Go to localhost:3000/");
+
+app.get("/phrases", function (req, res){
+  // render phrases index as JSON
+  res.send(JSON.stringify(phrases));
+});
+
+app.post("/phrases", function (req, res){
+  console.log("made it to app.post")
+  var newPhrase = req.body.newPhrase;
+  phrases.push(newPhrase);
+  console.log(newPhrase);
+  res.send(newPhrase);
+});
+
+// listen on port 3000
+app.listen(3000, function (){
+  console.log("listening on port 3000");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,31 @@
 //set up a view object to render our content
 //set up other object models
 //actually initialize the page look (document.ready)
+// on page load
+$(function(){
+  // get and render the food
+  phrase.all();
+});
+
+// // // // // // //
+
+// VIEW OBJECT
+function View() {};
+View.render = function(items, parentId, templateId) {
+  // render a template
+  var compile = _.template($("#" + templateId).html());
+  // input data into template and append to parent
+  $("#" + parentId).html(compile({collection: items}));
+};
+
+// Phrases OBJECT
+function phrase() {};
+phrase.all = function() {
+  $.get("/phrases", function(res){ 
+    // parse the response
+    var phrases = JSON.parse(res);
+    console.log(phrases);
+    // render the results
+    View.render(phrases, "phrase-ul", "phrases-template");
+  });
+};
